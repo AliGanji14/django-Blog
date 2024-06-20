27f8d7662e79f0391da57a64349a8cecf2bd95bc
Ali Ganji <aliganji142879@gmail.com>
2024-06-20 16:27:16 +0330

    post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('', views.post_list_view, name='posts_list'),
+    path('', views.post_list_view, name='post_list'),
     path('<int:pk>/', views.post_detail_view, name='post_detail'),
+    path('create/', views.post_new_view, name='post_create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from .models import Post
 
 
 def post_list_view(request):
-    posts_list = Post.objects.all()
+    posts_list = Post.objects.filter(status='pub').order_by('datetime_modified')
     return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
 
 
 def post_detail_view(request, pk):
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new_view(request):
+    if request.method == "POST":
+        post_text = request.POST.get('title')
+        post_title = request.POST.get('text')
+        Post.objects.create(title=post_title, text=post_text,
+                            author=None, stauts='pub')
+    else:
+        return render(request, 'blog/post_create.html')
